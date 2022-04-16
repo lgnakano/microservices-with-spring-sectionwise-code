4ee21ef46f02895e67e0bd8640d7bf7603281ee8
Luis Nakano <lgnakano@gmail.com>
2022-04-16 17:59:58 -0400

    Use retry without fallback.

--- section9/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section9/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -70,24 +70,26 @@ public class AccountsController {
 	}
 	
 	@PostMapping("/myCustomerDetails")
-	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
-//	@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
+//	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
+	@Retry(name = "retryForCustomerDetails"
+//			, fallbackMethod = "myCustomerDetailsFallBack"
+	)
 	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) throws LoansFeignClientException, CardsFeignClientException {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans;
-		try {
+//		try {
 			loans = loansFeignClient.getLoansDetails(customer);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new LoansFeignClientException(e.getMessage());
-		}
+//		} catch (Exception e) {
+////			e.printStackTrace();
+//			throw new LoansFeignClientException(e.getMessage());
+//		}
 		List<Cards> cards;
-		try {
+//		try {
 			cards = cardsFeignClient.getCardDetails(customer);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new CardsFeignClientException(e.getMessage());
-		}
+//		} catch (Exception e) {
+////			e.printStackTrace();
+//			throw new CardsFeignClientException(e.getMessage());
+//		}
 
 		CustomerDetails customerDetails = new CustomerDetails();
 		customerDetails.setAccounts(accounts);
