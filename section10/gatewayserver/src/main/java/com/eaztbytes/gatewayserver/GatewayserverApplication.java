f43ff6a5d275fa6915209708d4234e9ee94f048f
Luis Nakano <lgnakano@gmail.com>
2022-04-17 18:41:31 -0400

    Allow call to gateway server including /eazybank and add response header with response time.

--- section10/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
@@ -17,24 +17,24 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 	
-//	@Bean
-//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//	    return builder.routes()
-//	        .route(p -> p
-//	            .path("/eazybank/accounts/**")
-//	            .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-//	            				.addResponseHeader("X-Response-Time",new Date().toString()))
-//	            .uri("lb://ACCOUNTS")).
-//	        route(p -> p
-//		            .path("/eazybank/loans/**")
-//		            .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-//		            		.addResponseHeader("X-Response-Time",new Date().toString()))
-//		            .uri("lb://LOANS")).
-//	        route(p -> p
-//		            .path("/eazybank/cards/**")
-//		            .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
-//		            		.addResponseHeader("X-Response-Time",new Date().toString()))
-//		            .uri("lb://CARDS")).build();
-//	}
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/eazybank/accounts/**")
+	            .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
+	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .uri("lb://ACCOUNTS")).
+	        route(p -> p
+		            .path("/eazybank/loans/**")
+		            .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://LOANS")).
+	        route(p -> p
+		            .path("/eazybank/cards/**")
+		            .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://CARDS")).build();
+	}
 
 }
